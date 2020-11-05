dce75e618886f810de4445145f2431a5bad108ee
Vishal Garg <vishugarg10004@gmail.com>
2020-11-05 20:11:34 +0530

    [Vishal] adding prices for weekend and calculating total prices

--- HotelReservationSystem/HotelModel.cs
+++ HotelReservationSystem/HotelModel.cs
@@ -4,15 +4,26 @@ using System.Text;
 
 namespace HotelReservationSystem
 {
+    /// <summary>
+    /// hotel model to define variables
+    /// </summary>
     public class HotelModel
     {
         public string hotelName;
         public int weekdayRegularRates;
+        public int weekendRegularRates;
 
-        public HotelModel(string hotelName,int weekdayRegularRates)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HotelModel"/> class.
+        /// </summary>
+        /// <param name="hotelName">Name of the hotel.</param>
+        /// <param name="weekdayRegularRates">The weekday regular rates.</param>
+        /// <param name="weekendRegularRates">The weekend regular rates.</param>
+        public HotelModel(string hotelName,int weekdayRegularRates, int weekendRegularRates)
         {
             this.hotelName = hotelName;
             this.weekdayRegularRates = weekdayRegularRates;
+            this.weekendRegularRates = weekendRegularRates;
         }
     }
 }
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace HotelReservationSystem
 {
@@ -8,22 +9,29 @@ namespace HotelReservationSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation Program");
-
+            //instatiating hotel reservations
             HotelReservation hotelReservation = new HotelReservation();
+            //calling method for adding hotels in list
             hotelReservation.AddingHotelsInList();
             Console.WriteLine("Please enter the dates for which cheapest hotel needs to be find out");
+            //reading the dates in string
             string datesString = Console.ReadLine();
+            //adding the string of dates in array
             string[] datesArray = datesString.Split(',');
+            //creating day of week dates list
             List<DayOfWeek> datesList = new List<DayOfWeek>();
+            //iterating over array of dates, converting it in date and then adding day of week  in list
             for (int i=0;i<datesArray.Length;i++)
             {
                DateTime date= Convert.ToDateTime(datesArray[i]);
                datesList.Add(date.DayOfWeek);
             }
+            //printing the days
             foreach(DayOfWeek day in datesList)
             {
                 Console.WriteLine(day);
             }
+            //calling the method for getting cheapest hotel for given dates
             hotelReservation.CheapestHotelForGivenDates(datesList);
         }
 
