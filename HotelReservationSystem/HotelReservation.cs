3b9f3a9ddad8a896bc57a8c3cb78822189d23e41
Vishal Garg <vishugarg10004@gmail.com>
2020-11-05 21:24:11 +0530

    [Vishal] added ratings for hotels

--- HotelReservationSystem/HotelReservation.cs
+++ HotelReservationSystem/HotelReservation.cs
@@ -13,15 +13,17 @@
         List<HotelModel> hotelsList = new List<HotelModel>();
         //making a  list for adding rates and hotel names
         List<HotelModel> rateAndHotelsList = new List<HotelModel>();
+        //defining a list to get a hotel with highest ratings after getting hotel with min rates
+        List<HotelModel> listWithMinPrices = new List<HotelModel>();
 
         /// <summary>
         /// Addings the hotels in list.
         /// </summary>
         public void AddingHotelsInList()
         {
-            hotelsList.Add(new HotelModel("Lakewood", 110,90));
-            hotelsList.Add(new HotelModel("Bridegewood", 150,50));
-            hotelsList.Add(new HotelModel("Ridgewood", 220,150));
+            hotelsList.Add(new HotelModel("Lakewood", 110,90,3));
+            hotelsList.Add(new HotelModel("Bridegewood", 150,50,4));
+            hotelsList.Add(new HotelModel("Ridgewood", 220,150,5));
             Console.WriteLine("Hotel Name \tWeekdayHotelPrices\tweekendHotelPrices");
             //printing the details of hotels
             foreach(HotelModel hotelModel in hotelsList)
@@ -59,7 +61,7 @@
                 Console.WriteLine(" Hotel for given Dates:\t" + hotelModel.hotelName + " Total Price to be paid:\t" +totalPrice);
                 //adding price and hotelname in sorted  dictionary
                 //will sorted by price
-                rateAndHotelsList.Add(new HotelModel(totalPrice,hotelModel.hotelName));
+                rateAndHotelsList.Add(new HotelModel(totalPrice,hotelModel.hotelName,hotelModel.ratingsForHotels));
             }
         }
         /// <summary>
@@ -71,14 +73,27 @@
             //calling calculatingpriceforeachhotel for totalrates for each hotel
             CalculatingPriceForEachHotel(datesList);
             //iterating the loop and breaking at first iteration
+            //using lambda expression and linq to order the list by total rates
             foreach(HotelModel hotelModel in rateAndHotelsList.OrderBy(r=>r.totalRate).ToList())
             {
+                //printing the hotels with minimum rates
                 if (hotelModel.totalRate == rateAndHotelsList.Min(r => r.totalRate))
                 {
-                    Console.WriteLine("\nCheapest Hotel for given Dates:\t" + hotelModel.hotelName + "\nTotal Price to be paid for cheapest hotel:\t" + hotelModel.totalRate);
+                    listWithMinPrices.Add(hotelModel);
+                    Console.WriteLine("\nHotel for given Dates:\t" +hotelModel.hotelName + "\nTotal Price to be paid for hotel:\t" + hotelModel.totalRate+"\nRating of Hotel:\t"+hotelModel.ratingsForHotels);
                     Console.WriteLine();
+
                 }
 
+            }
+            foreach(HotelModel hotelModel in listWithMinPrices)
+            {
+                if(hotelModel.ratingsForHotels==listWithMinPrices.Max(r=>r.ratingsForHotels))
+                {
+                    Console.WriteLine("*************************Cheapest hotel with best ratings*****************************");
+                    Console.WriteLine("\nCheapest Hotel for given Dates:\t" +hotelModel.hotelName + "\nTotal Price to be paid for cheapest hotel:\t" + hotelModel.totalRate + "\nRating of Hotel:\t" + hotelModel.ratingsForHotels);
+                    Console.WriteLine();
+                }
             }
         }
     }
