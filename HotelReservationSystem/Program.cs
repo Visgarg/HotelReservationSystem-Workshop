1d5c9155168475532968f1407eaa5352eb716c9d
Vishal Garg <vishugarg10004@gmail.com>
2020-11-05 17:30:33 +0530

    [Vishal] Welcome to Hotel Reservation Program

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HotelReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime dateValue = new DateTime(2008, 6, 14);
+            Console.WriteLine((int)dateValue.DayOfWeek);
+        }
+    }
+}
